3c4e9c7620f99e31ce62d8965b6a105f3255b2e5
DESKTOP-MMMPRLS\user <sean2684@naver.com>
2024-04-14 12:11:02 +0900

    <feat: 로고 이미지 적용>

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View, Dimensions } from 'react-native';
+import { Image, StyleSheet, Text, View, Dimensions, useWindowDimensions } from 'react-native';
 import { SimpleLineIcons } from '@expo/vector-icons';
+import { useEffect, useState } from 'react';
 const sticker1 = require('./assets/image/sticker_picture_01.jpg');
 const sticker2 = require('./assets/image/sticker_picture_02.jpg');
+const logo = require('./assets/image/Luv.png');
 
 export default function App() {
 
-  const { width } = Dimensions.get("window");
-  const tablets = width <= 1000
+  const { width, height } = useWindowDimensions();
+  const layout = width <= 1320;
+  const tablets = width <= 1000;
   const mobile = width <= 500;
   
   return (
     <View style={styles.container}>
       <View style={styles.love_title_container}>
-        <Text style={styles.header_wrapper}>
-          러브 카메라
+        <Text style={(layout) ? styles.header_layout : styles.header_wrapper}>
+          <Image source={logo} alt="로고" style={styles.logo} />
           <SimpleLineIcons
             name="menu"
             size={24}
@@ -23,8 +26,8 @@ export default function App() {
         </Text>    
       </View>
       <View style={(tablets) ? styles.image_wrapper : styles.image_wrapper_row}>
-        <Image source={sticker1} style={(tablets) ? styles.image_width_100 : styles.image_width_50} />
-        <Image source={sticker2} style={(tablets) ? styles.image_width_100 : styles.image_width_50} />
+        <Image source={sticker1} alt="배너 이미지1" style={(tablets) ? styles.image_width_100 : styles.image_width_50} />
+        <Image source={sticker2} alt="배너 이미지2" style={(tablets) ? styles.image_width_100 : styles.image_width_50} />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -50,12 +53,23 @@ const styles = StyleSheet.create({
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#71f2b2",
+    backgroundColor: "#ffc7ef",
     color: "#fff",
     zIndex: 5
   },
 
   header_wrapper: {
+    width: 1320,
+    height: "100%",
+    fontSize: 28,
+    fontWeight: "800",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    color: "#fff"
+  },
+
+  header_layout: {
     width: "96%",
     height: "100%",
     fontSize: 28,
@@ -64,6 +78,13 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "center",
     color: "#fff"
+  },
+
+  logo: {
+    width: 134,
+    height: 40,
+    objectFit: "cover",
+    pointerEvents: "auto"
   },
 
   image_wrapper: {
